51a8fb76ba25d870c59d83d4d70c9a187064bd49
Frederik Thomsen <vestervang-rs@hotmail.com>
2019-04-10 19:17:23 +0200

    Added solution for problem 2

---
+++ Problem2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new[] {10, 3, 5, 6, 2};
+
+            var products = findProduct(numbers);
+
+            foreach (var product in products)
+            {
+                Console.WriteLine(product);
+            }
+        }
+
+        private static IEnumerable<int> findProduct(IEnumerable<int> numbers)
+        {
+            var enumerable = numbers as int[] ?? numbers.ToArray();
+            var result = new int[enumerable.Length];
+
+            for (var i = 0; i < enumerable.Length; i++)
+            {
+                var product = enumerable.Where((t, j) => i != j).Aggregate(1, (current, t) => current * t);
+
+                result[i] = product;
+            }
+
+            return result;
+        }
+    }
+}
